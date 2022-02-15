461012ce6a032e212b3d036fdef91fb89773cd4f
ifeanyi Onyia-odike <ifex370@gmail.com>
2022-02-15 16:32:46 +0100

    New Jenks Build

--- jenkins-config.sh
+++ jenkins-config.sh
@@ -3,6 +3,6 @@ app_tag=`git ls-remote https://github.com/ifex370/node-app HEAD | awk '{print $1
 docker_app="08183530115/nodejenkins:$app_tag"
 docker build -t $docker_app .
 
-docker login -u 08183530115 -p Grandta6$ 
+docker login -u 08183530115 -p  
 
 docker push $docker_app
