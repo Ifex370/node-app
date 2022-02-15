bac9f45c1864c357c4c132f3fe9927416b71bb4c
ifeanyi Onyia-odike <ifex370@gmail.com>
2022-02-15 18:20:33 +0100

    New Jenks Build

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:green;">Large Systems Java Application for St2 - version-10!!</h1> \n');
+  res.send('<h1 style="color:green;">Large Systems Java Application for St2 - version-11!!</h1> \n');
 });
 
 app.listen(PORT, HOST);
